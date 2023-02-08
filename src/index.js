ff786c810c94923c96bfa70a995d5bfce7cf91cc
bainningking <70629146+bainningking@users.noreply.github.com>
2023-02-08 16:58:38 +0800

    delay(5000))尝试使每个语句发送时间变长

--- src/index.js
+++ src/index.js
@@ -62,7 +62,7 @@
                 getRandomMsg(dialog.details)
                     .forEach(content => {
                         this.msgChain = this.msgChain
-                            .then(() => delay(700))
+                            .then(() => delay(5000))
                             .then(() => this.sendMsg(content, AUTHOR.AUTHOR));
                     });
 
